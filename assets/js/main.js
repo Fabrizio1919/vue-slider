0d5d1803447280acb76b8bf516c48ddd71374605
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-03-29 18:38:26 +0200

    Add mouseover and mouseleave

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,40 @@
 const { createApp } = Vue
 
-  createApp({
+createApp({
     data() {
-      return {
-        activeImage:0,
-        images: [
-            {
-                image: './assets/img/01.webp',
-                title: 'Marvel\'s Spiderman Miles Morale',
-                text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-            }, {
-                image: './assets/img/02.webp',
-                title: 'Ratchet & Clank: Rift Apart',
-                text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-            }, {
-                image: './assets/img/03.webp',
-                title: 'Fortnite',
-                text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-            }, {
-                image: './assets/img/04.webp',
-                title: 'Stray',
-                text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-            }, {
-                image: './assets/img/05.webp',
-                title: "Marvel's Avengers",
-                text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-            }
-        ]
-      }
+        return {
+            idInterval: null,
+            activeImage: 0,
+            images: [
+                {
+                    image: './assets/img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                }, {
+                    image: './assets/img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                }, {
+                    image: './assets/img/03.webp',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                }, {
+                    image: './assets/img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                }, {
+                    image: './assets/img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                }
+            ]
+        }
     },
     methods: {
         nextImages() {
             console.log('next');
             this.activeImage++
-            if (this.activeImage === this.images.length){
+            if (this.activeImage === this.images.length) {
                 this.activeImage = 0
             }
         },
@@ -52,13 +53,17 @@ const { createApp } = Vue
             }
         },
         autoImg() {
-            setInterval(() => {
+            this.idInterval = setInterval(() => {
                 this.nextImages()
-            }, 3000);
+            }, 1000);
+        },
+        stopImg() {
+            clearInterval(this.idInterval)
         }
     },
     created() {
         this.autoImg()
     }
-    
-  }).mount('#app')
+
+
+}).mount('#app')
